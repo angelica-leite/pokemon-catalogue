ab60597b5bb1ecd31e87ddc85d6f64ca2d2cf117
Angélica <angeelikasl@gmail.com>
2022-08-10 17:55:05 -0300

    fix: page Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import Providers from "../contexts";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <Component {...pageProps} />;
+    </Providers>
+  );
 }
 
 export default MyApp;
