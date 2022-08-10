8ab0a68c954cd0694e1b981579f4108e4f18a61e
Angélica <angeelikasl@gmail.com>
2022-08-10 17:57:07 -0300

    feat: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,39 +2,40 @@ import Image from "next/image";
 import Link from "next/link";
 import LogoPokemon from "../../../public/assets/logoPokemon.png";
 import Pokebola from "../../../public/assets/pokebola.png";
+import styles from "../../../styles/Home.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <div>
-          <Image
-            src={Pokebola}
-            alt="Bola pokebola do Pokemon"
-            width={100}
-            height={50}
-          />
+    <header className={styles.header}>
+      <div>
+        <Image
+          src={Pokebola}
+          alt="Bola pokebola do Pokemon"
+          width={110}
+          height={60}
+        />
 
-          <Image
-            src={LogoPokemon}
-            alt="Logomarca Pokemon"
-            width={200}
-            height={100}
-          />
-        </div>
-        <ul>
-          <li>
+        <Image
+          src={LogoPokemon}
+          alt="Logomarca Pokemon"
+          width={200}
+          height={100}
+        />
+      </div>
+      <div>
+        <ul className={styles.links}>
+          <li className={styles.item}>
             <Link href="/">
               <a>Home</a>
             </Link>
           </li>
-          <li>
+          <li className={styles.item}>
             <Link href="/about">
               <a>Sobre</a>
             </Link>
           </li>
         </ul>
-      </nav>
+      </div>
     </header>
   );
 }
